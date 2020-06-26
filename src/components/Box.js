9909604981bcf05a4a70af989a1cacb065214c03
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2020-06-26 13:59:34 -0500

    Updated box to show number of each recipe

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,31 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
+import '../css/reset.css';
 
-const BoxWrapper = styled.div `
-	padding: 10px;
-	border: 1px solid black;
-	background: #ccc;
+const BoxWrapper = styled.div`
+  padding: 10px;
+  border: 1px solid black;
+  background: #ccc;
 `;
 
-const BoxContent = styled.div `
-	padding: 10px;
-	border: 1px dotted red;
-	background: #888;
-`
+const BoxContent = styled.div`
+  padding: 10px;
+  border: 1px dotted red;
+  background: #888;
+`;
 
 class Box extends React.Component {
-	render() {
-		const order = Object.keys(this.props.contents);
+  renderOrder = (key) => {
+    const count = this.props.contents[key];
+    const recipe = this.props.recipes[key];
+    for (let i = 0; i < count; i++) {
+      return <li key={key + i}>{recipe.title}</li>;
 
-		return (
-			<BoxWrapper>
-				<BoxContent>
-					{/* just added functionality to render the box state into the box. need to change it to add the total of each one, plus the price. */}
-					{order}
-				</BoxContent>
-			</BoxWrapper>
-		);
-	}
+      // This is currently only adding the items once to the box (visually, state is correct).
+    }
+  };
+
+  render() {
+    const orderIds = Object.keys(this.props.contents);
+
+    // OOPS. Wrote the box to make a count of each item on a single line, user story asks for each item to appear indiviudually. A recipe added twice will appear two times.
+
+    return (
+      <BoxWrapper>
+        <BoxContent>{orderIds.map(this.renderOrder)}</BoxContent>
+      </BoxWrapper>
+    );
+  }
 }
 
 export default Box;
--- src/components/Picker.js
+++ src/components/Picker.js
@@ -2,63 +2,65 @@ import React from 'react';
 import styled from 'styled-components';
 import RecipeCard from './RecipeCard';
 import sampleRecipes from '../recipes';
-import '../css/picker.css';
 import Box from './Box';
+import '../css/picker.css';
 
 const Button = styled.button`
-	border: none;
-	background: green;
-	padding: 10px;
-	color: white;
-	font-size: 1.3rem;
-	cursor: pointer;
-	margin: 0 auto;
-	border-radius: 10px;
-`
-const Wrapper = styled.div `
-	display: grid;
-	grid-template-columns: 3fr 1fr;
-
-`
+  border: none;
+  background: green;
+  padding: 10px;
+  color: white;
+  font-size: 1.3rem;
+  cursor: pointer;
+  margin: 0 auto;
+  border-radius: 10px;
+`;
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+`;
 
 class Picker extends React.Component {
-	state = {
-		recipes: {},
-		box: {},
-	}
+  state = {
+    recipes: {},
+    box: {},
+  };
 
-	loadSampleRecipes = () => {
-		this.setState({recipes: sampleRecipes});
-	}
+  loadSampleRecipes = () => {
+    this.setState({ recipes: sampleRecipes });
+  };
 
-	addToBox = (key) => {
-		const box = {...this.state.box};
-		box[key] = box[key] + 1 || 1;
-		this.setState({box});
-	}
+  addToBox = (key) => {
+    const box = { ...this.state.box };
+    box[key] = box[key] + 1 || 1;
+    this.setState({ box });
+    // I think I can leave this as is, and use the value of each key to tell react how many of that item to render
+  };
 
-	render() {
-		const keys = Object.keys(this.state.recipes);
+  render() {
+    const keys = Object.keys(this.state.recipes);
 
-		return (
-			<Wrapper>
-				<div className="test">
-					{keys.map(key => 
-						<RecipeCard
-							key={key}
-							index={key}
-							title={this.state.recipes[key].title}
-							image={this.state.recipes[key].image}
-							protein={this.state.recipes[key].protein}
-							addToBox={this.addToBox}
-						/>)
-					}
-				</div>
-				{keys.length === 0 ? <Button onClick={this.loadSampleRecipes}>Load Sample Recipes</Button> : null}
-				<Box contents={this.state.box}/>
-			</Wrapper>
-		);
-	}
+    return (
+      <Wrapper>
+        <div className="test">
+          {keys.map((key) => (
+            <RecipeCard
+              key={key}
+              index={key}
+              title={this.state.recipes[key].title}
+              image={this.state.recipes[key].image}
+              protein={this.state.recipes[key].protein}
+              addToBox={this.addToBox}
+            />
+          ))}
+        </div>
+        {keys.length === 0 ? (
+          <Button onClick={this.loadSampleRecipes}>Load Sample Recipes</Button>
+        ) : null}
+        <Box contents={this.state.box} recipes={this.state.recipes} />
+      </Wrapper>
+    );
+  }
 }
 
 export default Picker;

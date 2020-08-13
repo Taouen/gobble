895dd5a3b36b0b6a5b90437a7ecd2a08574057d2
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2020-08-13 17:55:43 -0500

    added filter button arrow

--- src/components/CategoryBar.js
+++ src/components/CategoryBar.js
@@ -20,7 +20,7 @@ const CategoriesMenu = styled.ul`
   transition: 0.5s;
   width: 100%;
   &.active {
-    height: 100px;
+    height: 6rem;
     overflow: scroll;
     /* need to figure out transition, currently even setting the transition does nothing. */
   }
@@ -33,7 +33,7 @@ const CategoryButton = styled.button`
   cursor: pointer;
   font-size: 1.2rem;
   margin: 5px;
-  padding: 10px; /* apparently the li has a default padding, need this to override */
+  padding: 5px 10px; /* apparently the li has a default padding, need this to override */
   /*   width: 100px;  */ /*  not sure what to set here, need all the buttons to be the same width but dont want to use a fixed width */
   &:hover {
     background: green;
@@ -42,6 +42,15 @@ const CategoryButton = styled.button`
 const FilterButton = styled.h4`
   font-size: 1.2rem;
   margin-bottom: 5px;
+`;
+const Arrow = styled.img`
+  width: 5vw;
+  transition: 0.3s;
+  transform: rotate(-180deg);
+  &.active {
+    transform: rotate(0deg);
+    transition: 0.3s;
+  }
 `;
 
 class Category extends React.Component {
@@ -59,7 +68,14 @@ class Category extends React.Component {
 
     return (
       <Bar>
-        <FilterButton onClick={this.openMenu}>Filter</FilterButton>
+        <FilterButton onClick={this.openMenu}>
+          Filter{' '}
+          <Arrow
+            src="/images/arrow.png"
+            className={this.state.active ? 'active' : 'null'}
+          />
+        </FilterButton>
+
         <CategoriesMenu className={this.state.active ? 'active' : 'null'}>
           {categories.map((protein, i) => (
             <li key={i}>
